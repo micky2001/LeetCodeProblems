b0e4c0b5736c1e305c41966e82df73bdbddb9aaf
Subhashree <47693419+micky2001@users.noreply.github.com>
2022-08-04 19:36:46 +0530

    Time: 7 ms (17.20%), Space: 7.1 MB (33.91%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
+    void rotate(vector<vector<int>>& matrix) 
+    {
+        /*
+        //Bruit Force:
+        
         int n = matrix.size();
         vector<vector<int>> res(n, vector<int>(n,0));
         
@@ -11,5 +15,21 @@ public:
         for(int i=0;i<n;i++)
             for(int j=0;j<n;j++)
                 matrix[i][j] = res[i][j];
+        */
+        
+        int n = matrix.size();
+        
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        
+        for(int i = 0; i < n; i++)
+        {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
     }
 };
